522047b858b7ccfdd2f2f7f9f5540285b1716add
Rolderek <restaslaci@gmail.com>
2026-07-09 18:41:04 +0200

    Otthon

---
+++ Gyakorlas/Pages/Notification/NotificationHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Gyakorlas.Pages.Notification
+
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendNotifitacion(TodoItem item)
+        {
+            await Clients.All.SendAsync("TodoChanged", item);
+        }
+
+    }
+}
