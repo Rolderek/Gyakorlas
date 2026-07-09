054a2a9ae07972309dfbe33f1ac8c90ba93a8ba9
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-07-09 15:27:11 +0200

    State

---
+++ Gyakorlas/Services/StateService.cs
@@ -0,0 +1,21 @@
+﻿namespace Gyakorlas.Services
+{
+    public class StateService
+    {
+        private List<string> messages = new List<string>();
+
+        public event Action OnChange;
+
+        public IEnumerable<string> GetMessage() => messages;
+
+        //ha változik akkor szóljál kérlek:
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        //tedd bele a listába az új elemet és szóljál hogy változott:
+        public void AddMessage(string m)
+        {
+            messages.Add(m);
+            NotifyStateChanged();
+        }
+    }
+}
